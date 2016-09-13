e53a11c169b5912bd1cf22c49d851e3e4f13c6f4
Michał Wawrzyniak <michalw@tangramcare.com>
2016-09-13 17:23:51 +0200

    unicode for model

--- apps/sample_app/models.py
+++ apps/sample_app/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class SampleModel(models.Model):
     text = models.CharField(default='', max_length=100)
     uploaded_file = models.FileField(upload_to='uploaded/')
+
+    def __unicode__(self):
+        return self.text
